4539c74f4a2646f21f896da1ee66651d213e3523
jouthoff <jouthoff@gmail.com>
2023-11-26 23:59:46 +0000

    Download fermentation runs from Notion API

--- src/training/read.py
+++ src/training/read.py
@@ -1,5 +1,69 @@
+import tempfile
+
+import gdown
 import pandas as pd
+import requests
+from pandas import read_csv
+
+TEMP_DIR = tempfile.TemporaryDirectory()
 
 
 def read_calibration_data(filepath: str) -> pd.DataFrame:
     return pd.read_csv(filepath)
+
+
+def read_fermentation_run_data(url):
+    tempfile_name = TEMP_DIR.name + url[-10:] + '.csv'
+    return read_csv(tempfile_name)
+
+
+def download_run_data():
+    runs = read_sop_data()
+    for run in runs:
+        url = run['url']
+        tempfile_name = TEMP_DIR.name + url[-10:] + '.csv'
+        download_file_from_url(run['url'], tempfile_name)
+    return runs
+
+
+def read_sop_data():
+    notion_token = 'secret_SBzckpdOOtWsu7R6BSt3rEMPP2XIrakgLogPOg9ItnW'
+    database_id = '558261863d3b40d2a2d403748079e37f'
+    url = f'https://api.notion.com/v1/databases/{database_id}/query'
+
+    r = requests.post(url, headers={
+        "Authorization": f"Bearer {notion_token}",
+        "Notion-Version": "2021-08-16"
+    })
+
+    result_dict = r.json()
+    result = result_dict['results']
+
+    fermentation_runs = []
+
+    for run in result:
+        fermentation_runs_dict = map_notion_result_to_fermentation_run(run)
+        fermentation_runs.append(fermentation_runs_dict)
+
+    return fermentation_runs
+
+
+def map_notion_result_to_fermentation_run(result):
+    # you can print result here and check the format of the answer.
+    properties = result['properties']
+
+    return {
+        'client': properties['Client']['select']['name'],
+        'date': properties['Date']['date']['start'],
+        'url': properties['Download URL']['url']
+    }
+
+
+def download_file_from_url(url: str):
+    tempfile_name = TEMP_DIR.name + url[-10:] + '.csv'
+    gdown.download(url, tempfile_name)
+
+
+def clean_up_tempfiles():
+    TEMP_DIR.cleanup()
+
--- test/training/test_read.py
+++ test/training/test_read.py
@@ -1,9 +1,36 @@
-from src.training.read import read_calibration_data
+from src.training.read import read_calibration_data, download_run_data, read_sop_data, download_file_from_url, TEMP_DIR, \
+    read_fermentation_run_data
 
 
 def test_read_calibration_data() -> None:
+    # Given a csv filepath
     calibration_data_filepath = '../../src/training/data/calibration.csv'
-
+    # When read_calibration_data is called
     output = read_calibration_data(calibration_data_filepath)
+    # Then the df has the correct number of rows
+    assert len(output) == 27
+
+
+def test_read_fermentation_run_data():
+    url = 'https://drive.google.com/uc?export=download&id=1BhIbqzmNth7rG-3HXRTibzhCZY6wMb3L'
+    download_file_from_url(url)
+
+    output = read_fermentation_run_data(url)
+
+    assert len(output) == 3
+
+
+def test_download_run_data():
+    output = download_run_data()
 
     assert len(output) == 27
+
+
+def test_read_dye_house_data() -> None:
+    output = read_sop_data()
+    assert len(output) == 27
+
+
+def test_download_file_from_url() -> None:
+    url = 'https://drive.google.com/uc?export=download&id=1BhIbqzmNth7rG-3HXRTibzhCZY6wMb3L'
+    download_file_from_url(url)
