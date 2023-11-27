26abaae815ad07283476530674852d23b1bbefdf
jouthoff <jouthoff@gmail.com>
2023-11-27 01:26:38 +0000

    Write predictions to json

--- src/write.py
+++ src/write.py
@@ -2,5 +2,5 @@ def write_model_to_json(model):
     model.to_json('calibrated_model.json')
 
 
-def write_predicted_concentrations_to_json(df):
-    df.to_json('predicted_')
+def write_predicted_concentrations_to_json(df, run):
+    df.to_json('/prediction_files/predicted_' + run['client'] + run['date'])
